1d2599138ae1cda9b0e8d749f8a8dbc67a9ca8df
B-Shankar <learn.bhimashankar@gmail.com>
2026-01-27 21:12:45 +0530

    3. Longest Substring Without Repeating Characters: Optimised

--- src/collections_problems/string_problems/LongestSubstring.java
+++ src/collections_problems/string_problems/LongestSubstring.java
@@ -3,7 +3,7 @@ package collections_problems.string_problems;
 import java.util.HashMap;
 import java.util.Map;
 
-// 3. Longest Substring Without Repeating Characters
+// 3. Longest Substring Without Repeating Characters [Medium]
 public class LongestSubstring {
 
     // O(n^2)
@@ -34,12 +34,34 @@ public class LongestSubstring {
         return max;
     }
 
+    // Time Complexity: O(n); Space Complexity: O(n)
+    public static int lengthOfLongestSubstringOptimised(String s) {
+        int left = 0, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < s.length(); right++) { // O(n)
+
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+
+            map.put(ch, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
     public static void main(String[] args) {
 //        String s = "abcabcbb";
 //        String s = "bbbbb";
         String s = "pwwkew";
 
         int ans = lengthOfLongestSubstring(s);
+        int ans1 = lengthOfLongestSubstringOptimised(s);
         System.out.println("ANS: " + ans);
+        System.out.println("ANS: " + ans1);
     }
 }
