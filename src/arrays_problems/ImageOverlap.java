14bc9282a66a55aaa8a11581890f724f17f5a659
B-Shankar <learn.bhimashankar@gmail.com>
2026-03-07 21:57:36 +0530

    835. Image Overlap

---
+++ src/arrays_problems/ImageOverlap.java
@@ -0,0 +1,58 @@
+package arrays_problems;
+
+// 835. Image Overlap
+public class ImageOverlap {
+
+    // nxn
+    static int largestOverlap(int[][] A, int[][] B) {
+        int n = A.length;
+
+        int maxOverlap = 0;
+
+        /*
+            // rows(2x2): -1, 0, 1
+            // cols(2x2): -1, 0, 1
+
+            (-n+1) => (n-1)
+        */
+        for (int row_offset = -(n-1); row_offset < n; row_offset++) {
+            for (int col_offset = -(n-1); col_offset < n; col_offset++) {
+
+                int count = overlapCount(A, B, row_offset, col_offset);
+                maxOverlap = Math.max(maxOverlap, count);
+            }
+        }
+
+        return maxOverlap;
+    }
+
+    static int overlapCount(int[][] A, int[][] B, int row_offset, int col_offset) {
+        int n = A.length;
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // A[i][j] => B[i+row_offset][j+col_offset]
+
+                int b_row = i + row_offset;
+                int b_col = j + col_offset;
+
+                if (b_row < 0 || b_row >= n || b_col < 0 || b_col >= n)
+                    continue;
+
+                if (A[i][j] == 1 && B[b_row][b_row] == 1)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] img1 = {{1,1,0}, {0,1,0}, {0,1,0}};
+        int[][] img2 = {{0,0,0}, {0,1,1}, {0,0,1}};
+
+        System.out.println(largestOverlap(img1, img2));
+    }
+}
