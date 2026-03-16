da851d64e1c9db45d2e2532472bfda734c6b5c68
B-Shankar <learn.bhimashankar@gmail.com>
2026-03-16 23:19:31 +0530

    238. Product of Array Except Self

---
+++ src/arrays_problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,34 @@
+package arrays_problems;
+
+import java.util.Arrays;
+
+// 238. Product of Array Except Self
+public class ProductOfArrayExceptSelf {
+
+    static public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+
+        // prefix products
+        for(int i = 1; i < n; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+
+        int right = 1;
+
+        // suffix products
+        for(int i = n-1; i >= 0; i--){
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
